1753e365cfe594652e6374374f3ac9c9e235ab4f
Boakye Awuah Nicholas <boakyeawuahnicholas@gmail.com>
2023-03-21 04:16:32 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		puthar(i + '0');
+	}
+	for (j = 'a' ; j <= 'f' ; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n')
+	return (0);
+}
